e85ea457c336c16053ba5dc45b14cf905654bf97
abhaydotsingh <abhay.singh@nutanix.com>
2024-08-29 16:32:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,23 +6,24 @@ import subprocess
 import threading
 
 
-app = Flask(__name__)
-
-DB_DIR = "data"
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_DIR = os.path.join(ROOT_DIR, "data")
 DB_FILE = "data.json"
 DB_FILE_PATH = os.path.join(DB_DIR, DB_FILE)
-RUNS_DIR = "runs"
+RUNS_DIR = os.path.join(ROOT_DIR, "runs")
+os.makedirs(DB_DIR, exist_ok=True)
 os.makedirs(RUNS_DIR, exist_ok=True)
 
+app = Flask(__name__, static_folder=RUNS_DIR)
 
-def _start_test_and_logging(probe, job_id):
+def _start_test_and_logging(probe, job_id, config_file_path, runs_dir=RUNS_DIR):
     logfile_name = f'{job_id}_output.log'  
     logfile_path = os.path.join(RUNS_DIR, logfile_name)
 
     # Open the logfile in write mode
     with open(logfile_path, 'w', encoding='utf-8') as logfile:
         process = subprocess.Popen(
-            f'python -m garak --model_type rest --probes {probe} -G {job_id}_config.json --config garak_config.yaml --report_prefix {job_id}',
+            f'python -m garak --model_type rest --probes {probe} -G {config_file_path} --config garak_config.yaml --report_prefix {job_id}',
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
             shell=True,
@@ -37,7 +38,7 @@ def _start_test_and_logging(probe, job_id):
                 break
 
             if realtime_output:
-                print(realtime_output.strip(), flush=True)  # Print to console
+                # print(realtime_output.strip(), flush=True)  # Print to console
                 logfile.write(realtime_output)  # Write to logfile
                 logfile.flush()  # Ensure the output is written to the file immediately
 
@@ -48,9 +49,9 @@ def home():
 
 @app.route('/start_test', methods=['POST'])
 def start_test():
-	# Step 1: Get 'probe' and 'config_json' from the request payload
+	# Step 1: Get 'dataset' and 'config_json' from the request payload
     data = request.get_json()
-    probe = data.get('probe')
+    probe = data.get('dataset')
     config_json = data.get('config_json')
     job_id = uuid4()
     
@@ -61,13 +62,13 @@ def start_test():
     config_file_path = os.path.join(DB_DIR, f'{job_id}_config.json')
     with open(config_file_path, 'w') as config_file:
         json.dump(config_json, config_file, indent=2)
-  
+
     new_data = {
         "job_id": str(job_id),
         "probe": probe,
-        "config_json_path": config_file_path,
+        "config_json_path": config_json,
         "results_file_path": os.path.join(RUNS_DIR, f"{job_id}.report.jsonl"),
-        "log_file_path": os.path.join(RUNS_DIR, f"{job_id}.log"),
+        "log_file_path": os.path.join(RUNS_DIR, f"{job_id}_output.log"),
         "total_prompts": 100,
         "passed_prompts": 70,
     }
@@ -84,21 +85,21 @@ def start_test():
     
     # Step 3: Start a test using garak with the 'probe' and 'config_json'
     # Run the process in a separate thread
-    thread = threading.Thread(target=_start_test_and_logging, args=(probe, job_id))
+    thread = threading.Thread(target=_start_test_and_logging, args=(probe, job_id, config_file_path))
     thread.start()
 
     # Step 4: Return a response saying "test started"
     return jsonify({"message": "test started", "job_id": job_id}), 200
 
 @app.route('/get_file/<filename>', methods=['GET'])
-def get_file(folder_name, filename):
+def get_file(filename):
     # Check if the file exists within the folder
     file_path = os.path.join(RUNS_DIR, filename)
     if not os.path.exists(file_path):
         return jsonify({"error": "File not found"}), 404
     
     # Send the file
-    return send_from_directory(RUNS_DIR, filename)
+    return send_from_directory(app.static_folder, filename)
 
 @app.route('/list', methods=['GET'])
 def list_jobs():
